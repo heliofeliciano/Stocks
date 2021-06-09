4667370629290417b8400e633a8e7c612be4ae2a
Helio Feliciano <heliofeliciano@gmail.com>
2021-06-08 21:32:24 -0300

    Create a StockList innto HomeMarket class

--- Stocks/Models/HomeMarket.cs
+++ Stocks/Models/HomeMarket.cs
@@ -1,8 +1,11 @@
-﻿namespace Stocks.Models
+﻿using System.Collections.Generic;
+
+namespace Stocks.Models
 {
     public class HomeMarket
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Stock> Stocks { get; set; }
     }
 }
