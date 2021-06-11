fe62dec2ebf5dfb01c45699ee1c2a9e840fd4774
Helio Feliciano <heliofeliciano@gmail.com>
2021-06-10 22:23:11 -0300

    Test with HttpClient - TODO

---
+++ Stocks/Libraries/HttpClientInstance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Stocks.Libraries
+{
+    public class HttpClientInstance
+    {
+
+        private string url;
+
+        public HttpClientInstance(string urlrequest)
+        {
+            url = urlrequest;
+        }
+
+        public async Task<string> getStringAsync()
+        {
+            using var httpClient = new HttpClient();
+            /*string resultado = await httpClient.GetStringAsync(url);*/
+
+            return await httpClient.GetStringAsync(url);
+        }
+    }
+}
