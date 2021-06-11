9def0a15a6ea36b9b80039100d5e768a9d2530df
Helio Feliciano <heliofeliciano@gmail.com>
2021-06-10 22:12:22 -0300

    create a method to get a some information direct

--- Stocks/Libraries/StatusInvest.cs
+++ Stocks/Libraries/StatusInvest.cs
@@ -5,23 +5,41 @@ using System.Threading.Tasks;
 
 namespace Stocks.Libraries
 {
-    public class StatusInvest
+    public static class StatusInvest
     {
-        public string PatternCurrentValue 
+        public static string PatternCurrentValue 
         {
             get { return @"Valor atual<\/h3>\s<span class=\""icon\"">R.<\/span>\s<strong class=\""value\"">(\d+,\d+)<\/strong>"; }
             private set { PatternCurrentValue = value;  }
         }
-        public string PatterParity
+        public static string PatterParity
         {
             get { return @"(\d)\sStock = (\d+)"; }
             private set { PatterParity = value; }
         }
 
-        public string Site
+        public static string Site
         {
             get { return "https://statusinvest.com.br/bdrs"; }
             private set { Site = value; }
+        }
+
+        public static string getCurrentValue(string content)
+        {
+            var resultado = RegularExpresion.GetMatches(content, PatternCurrentValue);
+            return resultado[0].Groups[1].Value;
+        }
+        
+        public static string getParityMainStock(string content)
+        {
+            var resultado = RegularExpresion.GetMatches(content, PatterParity);
+            return resultado[0].Groups[1].Value;
+        }
+        
+        public static string getParityBDRStock(string content)
+        {
+            var resultado = RegularExpresion.GetMatches(content, PatterParity);
+            return resultado[0].Groups[2].Value;
         }
     }
 }
