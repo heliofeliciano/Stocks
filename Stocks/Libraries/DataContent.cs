a9bffe7c1d1e772631fd260ca2e24960f1d37b51
Helio Feliciano <heliofeliciano@gmail.com>
2021-06-11 22:19:43 -0300

    Create a DataContent with some exames of objects

---
+++ Stocks/Libraries/DataContent.cs
@@ -0,0 +1,48 @@
+﻿using Stocks.Models;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Stocks.Libraries
+{
+    public class DataContent
+    {
+        public List<Stock> GetStocks()
+        {
+            var list = new List<Stock>();
+            list.Add(new Stock()
+            {
+                Id = 1,
+                Company = new Company()
+                {
+                    Id = 1,
+                    Name = "PETROBRAS"
+                },
+                Ticker = "PETR4"
+            });
+
+            list.Add(new Stock()
+            {
+                Id = 2,
+                Company = new Company()
+                {
+                    Id = 1,
+                    Name = "GUARARAPES"
+                },
+                Ticker = "GUAR3"
+            });
+            
+            list.Add(new Stock()
+            {
+                Id = 2,
+                Company = new Company()
+                {
+                    Id = 1,
+                    Name = "GUARARAPES"
+                },
+                Ticker = "GUAR3"
+            });
+
+            return null;
+        }
+    }
+}
