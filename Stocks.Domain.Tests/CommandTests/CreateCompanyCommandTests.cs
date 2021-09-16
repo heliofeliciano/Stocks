e6adba853734968dd488c57c02e41ca1573062d8
= <=>
2021-09-16 09:27:05 -0300

    CreateCompany tests

---
+++ Stocks.Domain.Tests/CommandTests/CreateCompanyCommandTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stocks.Domain.Commands.Company;
+
+namespace Stocks.Domain.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateCompanyCommandTests
+    {
+        private readonly CreateCompanyCommand _validCommand = new CreateCompanyCommand("APPLE", "12345678912345");
+        private readonly CreateCompanyCommand _invalidCommand = new CreateCompanyCommand("", "12345678912345");
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            _invalidCommand.Validate();
+
+            Assert.AreEqual(false, _invalidCommand.Valid);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido()
+        {
+            _validCommand.Validate();
+
+            Assert.AreEqual(true, _validCommand.Valid);
+        }
+    }
+}
